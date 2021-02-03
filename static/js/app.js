e51ea1a0acf50c15ad18f99007f33c9b558cb076
Nahid <nahidfarzad8751@gmail.com>
2021-02-03 09:38:16 -0500

    x

--- static/js/app.js
+++ static/js/app.js
@@ -1,65 +1,45 @@
 // from data.js
-var tabledata = data;
-
-
+var tableData = data;
 //refrences 
 var tbody = d3.select("tbody");
-
-function buildTable (data) {
+function buildTable(data) {
     tbody.html("");
     data.forEach((dataRow) => {
-
-
-
-    d3.select("body")
-        .selectAll("tr")
-         .data(data)
-        .enter()
-        .append("tr")
-
-    .html((item)=>{
-    return `<td>${}</td>`
-});
-
-
-
+    var row = tbody.append("tr");
+    //add to table 
+    Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+          cell.text(val);
+        }
+      );
+    });
+}
 function handleClick(){
-    // Prevent from Refreshing
-    d3.event.preventDefault();
+    // // Prevent from Refreshing
+    // d3.event.preventDefault();
     // Choose Input
     let date = d3.select("#datetime").property("value");
-    let filteredData = data;
-
-
-
-// no matching results
-if (results.length === 0) {
-    tbody.text(`No sightings on ${inputDate}.`);
-}
-// matching results 
-else {
-var AddData = (input) => {
-    input.forEach(ufoSightings => {
-        var row = $tbody.append("tr");
-        columns.forEach(column => row.append("td").text(ufoSightings[column])
-        )
-    });
-}}
-AddData(data);
-
-
-
-
-if(date) {
-   
-    filteredData = filteredData.filter((row) => row.datetime === date);
-}
-    // Table with filtered data
+    let filteredData = tableData;
+// // no matching results
+// if (results.length === 0) {
+//     tbody.text(`No sightings on ${inputDate}.`);
+// }
+// // matching results 
+// else {
+// var AddData = (input) => {
+//     input.forEach(ufoSightings => {
+//         var row = $tbody.append("tr");
+//         columns.forEach(column => row.append("td").text(ufoSightings[column])
+//         )
+//     });
+// }}
+// AddData(data);
+    if(date) {
+        filteredData = filteredData.filter((row) => row.datetime === date);
+    }
+        // Table with filtered data
     buildTable(filteredData);
-
-
-
+}
 d3.selectAll("#filter-btn").on("click", handleClick);
-
 // Table with data.js 
 buildTable(tableData);
